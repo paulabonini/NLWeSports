be372da34a51e516678004edde8120ca5c6851e7
Paula Bonini <paula_almeidabonini@hotmail.com>
2022-09-12 22:01:32 -0300

    First day- starting backend, frontend and mobile aplications

---
+++ mobile/App.tsx
@@ -0,0 +1,35 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>{props.title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello world!</Text>
+
+      <Button title="Send 1" />
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
